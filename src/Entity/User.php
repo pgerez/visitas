d55d36679b1ac9ec360025aa49bf2a48a3d1e1ae
Pablo <gerezpo@gmail.com>
2021-02-05 12:05:51 -0300

    Piramide Uploader PHP Class

---
+++ src/Entity/User.php
@@ -0,0 +1,26 @@
+<?php
+
+// src/Entity/User.php
+namespace App\Entity; 
+
+use Doctrine\ORM\Mapping as ORM;
+use Sonata\UserBundle\Entity\BaseUser;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="fos_user_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    protected $id;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
